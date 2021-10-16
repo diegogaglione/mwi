2160e07ccb3fd0ddeca5fa5fb78c0f728b26c9e2
Diego <72506891+diegogaglione@users.noreply.github.com>
2021-10-16 16:43:52 +0200

    Add files via upload

---
+++ manual-win-installer.py
@@ -0,0 +1,8 @@
+import zipfile
+from tkinter import filedialog
+import os
+file_path = filedialog.askopenfilename()
+program_path = os.path.basename(file_path).rsplit(".", 1 )[0]
+path_for_program = r"C:\\Users\\Diego\\Download\\Porco\\" + program_path
+with zipfile.ZipFile(file_path, 'r') as zip_ref:
+    zip_ref.extractall(path_for_program)
