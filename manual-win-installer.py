f1039ffa7461f1b4dba7b758274193c8e5764c1e
Diego <72506891+diegogaglione@users.noreply.github.com>
2021-10-16 16:48:38 +0200

    Path modified

--- manual-win-installer.py
+++ manual-win-installer.py
@@ -3,6 +3,6 @@ from tkinter import filedialog
 import os
 file_path = filedialog.askopenfilename()
 program_path = os.path.basename(file_path).rsplit(".", 1 )[0]
-path_for_program = r"C:\\Users\\Diego\\Download\\Porco\\" + program_path
+path_for_program = r"C:\\ProgramFiles\\" + program_path
 with zipfile.ZipFile(file_path, 'r') as zip_ref:
     zip_ref.extractall(path_for_program)
